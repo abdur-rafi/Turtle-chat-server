86a62f9b04df090ad535ef30887574f869084b60
abdur-rafi <abdurrafi403@gmail.com>
2021-02-20 15:04:31 +0600

    minor changes

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var auth = require('../auth');
 var path = require('path');
 /* GET home page. */
 router.get('/',function(req, res, next) {
-  res.sendFile(path.join(__dirname , '../public/login.html'));
+  res.status(200).json({message : "visit netlify"})
 });
 router.get('/main.html',auth.isAuthenticated, function(req, res, next) {
   res.sendFile(path.join(__dirname , '../public/main.html'));
